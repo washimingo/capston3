e3a062c054c5b30e9df09b58e9068b0917c02ad6
Johao <joha.marchant@duocuc.cl>
2025-08-20 00:15:51 -0400

    Proyecto Base "blank" Ionic

---
+++ SAF/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SAF',
+  webDir: 'www'
+};
+
+export default config;
