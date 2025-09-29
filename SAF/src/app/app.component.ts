4bc49acd6496f3571e27e9487063a9353461ebf1
deps-182 <116619823+deps-182@users.noreply.github.com>
2025-09-29 23:40:39 +0000

    Modificación de la interfaz de usuario y corrección de errores.

--- SAF/src/app/app.component.ts
+++ SAF/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { addIcons } from 'ionicons';
 import { documentTextOutline, timeOutline, checkmarkCircleOutline, closeCircleOutline, calendarOutline, alertCircleOutline, warningOutline, homeOutline, checkmarkDoneOutline, cubeOutline, barChartOutline, peopleOutline, settingsOutline, businessOutline, createOutline, trashOutline, addCircleOutline, listOutline, closeCircle, searchOutline, pieChartOutline, helpCircleOutline, analyticsOutline, helpCircle } from 'ionicons/icons';
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
-import { IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonContent, IonItem, IonIcon } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonContent, IonItem, IonIcon, IonSplitPane, IonButton, IonMenuToggle, IonButtons } from '@ionic/angular/standalone';
+import { MenuController } from '@ionic/angular';
 import { HeaderComponent } from './components/header/header.component';
 @Component({
   selector: 'app-root',
@@ -18,11 +19,16 @@ import { HeaderComponent } from './components/header/header.component';
     IonContent,
     IonItem,
     RouterModule,
-  IonIcon
+    IonIcon,
+    IonSplitPane,
+    IonButton,
+    IonMenuToggle,
+    IonButtons
   ],
 })
-export class AppComponent {
-  constructor(private router: Router) {
+export class AppComponent implements AfterViewInit {
+  @ViewChild(IonMenu) menu?: IonMenu;
+  constructor(private router: Router, private menuCtrl: MenuController) {
     addIcons({homeOutline,documentTextOutline,checkmarkDoneOutline,cubeOutline,barChartOutline,analyticsOutline,peopleOutline,settingsOutline,helpCircleOutline,businessOutline,pieChartOutline,timeOutline,checkmarkCircleOutline,closeCircleOutline,calendarOutline,alertCircleOutline,warningOutline,createOutline,trashOutline,addCircleOutline,listOutline,closeCircle,searchOutline});
     // Aplicar modo oscuro si está guardado en localStorage
     const dark = localStorage.getItem('saf-dark-mode');
@@ -31,6 +37,18 @@ export class AppComponent {
     } else {
       document.body.classList.remove('dark');
     }
+  this.setMenuType();
+  window.addEventListener('resize', this.setMenuType.bind(this));
+  }
+  
+  menuType: 'overlay' | 'side' = 'side';
+  
+  setMenuType() {
+    this.menuType = window.innerWidth <= 1024 ? 'overlay' : 'side';
+  }
+
+  ngAfterViewInit() {
+    this.setMenuType();
   }
   // Método eliminado para evitar recargas y mantener SPA
 }
